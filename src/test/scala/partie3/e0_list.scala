ab1dc84b3ef422ee604f8f9c4460341d21fa9d7c
Marc Karassev <marc.karassev@lizeo-group.com>
2018-07-30 10:34:47 +0200

    Partie 3.

--- src/test/scala/partie3/e0_list.scala
+++ src/test/scala/partie3/e0_list.scala
@@ -1,27 +1,25 @@
 package cons_et_nil
 
-
 import support.HandsOnSuite
-
 
 class e0_list extends HandsOnSuite {
 
   /*
-  * Ici on va définir une liste de manière récursive, comme une liste chainée
-  * Ainsi, une liste est soit la liste vide, soit un élément suivi d'une autre liste
-  */
+   * Ici on va définir une liste de manière récursive, comme une liste chainée
+   * Ainsi, une liste est soit la liste vide, soit un élément suivi d'une autre liste
+   */
   sealed trait List[+A] {
 
-    def map[B](fonction:A => B):List[B]
+    def map[B](fonction: A => B): List[B]
 
-    def flatMap[B](fonction:A => List[B]):List[B]
+    def flatMap[B](fonction: A => List[B]): List[B]
 
-    def filter(fonction:A => Boolean):List[A]
+    def filter(fonction: A => Boolean): List[A]
 
-    final def union[B >: A](list:List[B]):List[B]= {
+    final def union[B >: A](list: List[B]): List[B] = {
       this match {
-        case Cons(head,tail) => Cons(head, ???)
-        case Nil => ???
+        case Cons(head, tail) => Cons(head, tail.union(list))
+        case Nil              => list
       }
     }
 
@@ -30,63 +28,67 @@ class e0_list extends HandsOnSuite {
 
   object List {
     // 'A*' signifie var args de A
-    def apply[A](values:A*):List[A] = {
+    def apply[A](values: A*): List[A] = {
       if (values.isEmpty) {
         Nil
       } else {
         // ':_*' permet d'étendre une list en var args
-        (Cons(values.head, List(values.tail:_*)))
+        Cons(values.head, List(values.tail: _*))
       }
     }
   }
+
   /**
-   * Cons veut dire Constructor, c'est lui qui permet de construire la liste en ajoutant un élément à la queue
-   */
-  case class Cons[A](head:A, tail:List[A]) extends  List[A] {
+    * Cons veut dire Constructor, c'est lui qui permet de construire la liste en ajoutant un élément à la queue
+    */
+  case class Cons[A](head: A, tail: List[A]) extends List[A] {
 
-    def map[B](fonction:A => B):List[B] = ???
-
+    def map[B](fonction: A => B): List[B] =
+      Cons(fonction(head), tail.map(fonction))
 
     /**
-     * l'implémentation de flatMap a besoin d'union
-     */
-    def flatMap[B](fonction:A => List[B]):List[B] = ???
+      * l'implémentation de flatMap a besoin d'union
+      */
+    def flatMap[B](fonction: A => List[B]): List[B] =
+      fonction(head).union(tail.flatMap(fonction))
 
-    def filter(fonction:A => Boolean):List[A] = ???
+    def filter(fonction: A => Boolean): List[A] =
+      if (fonction(head)) Cons(head, tail.filter(fonction))
+      else tail.filter(fonction)
 
   }
 
   /**
-   * il y a qu'un seul Nil, donc cela peut être un case object
-   */
+    * il y a qu'un seul Nil, donc cela peut être un case object
+    */
   case object Nil extends List[Nothing] {
     type A = Nothing
 
-    def map[B](fonction:A => B):List[B]  = ???
+    def map[B](fonction: A => B): List[B] = Nil
 
-    def flatMap[B](fonction:A => List[B]):List[B] = Nil
+    def flatMap[B](fonction: A => List[B]): List[B] = Nil
 
-    def filter(fonction:A => Boolean):List[A] = ???
+    def filter(fonction: A => Boolean): List[A] = Nil
   }
 
   exercice("création") {
 
     List() should be(Nil)
 
-    List(1,2,3) should be(Cons(1,Cons(2,Cons(3,Nil))))
+    List(1, 2, 3) should be(Cons(1, Cons(2, Cons(3, Nil))))
 
   }
 
   exercice("map") {
-    List(1,2,3).map(x => x + 1) should be(List(2,3,4))
+    List(1, 2, 3).map(x => x + 1) should be(List(2, 3, 4))
   }
-
 
   exercice("union") {
 
-    List(1,2,3).union(List(4,5)) should be(List(1,2,3,4,5))
+    List(1, 2, 3).union(List(4, 5)) should be(List(1, 2, 3, 4, 5))
 
-    List(1,2,3).union(List("A","B","C")) should be(List(1,2,3,"A","B","C"))
+    List(1, 2, 3).union(List("A", "B", "C")) should be(
+      List(1, 2, 3, "A", "B", "C"))
     // Cet example est un peu complexe, le compilateur cherche un B pour l'union tel que
     // String <: B et A (ici Int) <: B
     // Le type qui convient est Any, l'équivalent de Object en Java
@@ -97,18 +99,17 @@ class e0_list extends HandsOnSuite {
 
   exercice("flatMap") {
 
-    val combinaison = for (a <- List("A","B"); i <- List(1,2)) yield (a + i)
+    val combinaison = for (a <- List("A", "B"); i <- List(1, 2)) yield (a + i)
 
-    combinaison should be( List("A1","A2","B1","B2"))
+    combinaison should be(List("A1", "A2", "B1", "B2"))
 
   }
 
   exercice("filter") {
 
-    List(1,2,3).filter(x => x > 1) should be(List(2,3))
+    List(1, 2, 3).filter(x => x > 1) should be(List(2, 3))
 
-    List(1,2,3).filter(x => false) should be(Nil)
+    List(1, 2, 3).filter(x => false) should be(Nil)
   }
-
 
 }
--- src/test/scala/partie3/e1_bonus_stream.scala
+++ src/test/scala/partie3/e1_bonus_stream.scala
@@ -6,127 +6,132 @@ class e1_bonus_stream extends HandsOnSuite {
 
   sealed trait Stream[+A] {
 
-    def map[B](fonction:A => B):Stream[B]
+    def map[B](fonction: A => B): Stream[B]
 
-    def flatMap[B](fonction:A => Stream[B]):Stream[B]
+    def flatMap[B](fonction: A => Stream[B]): Stream[B]
 
-    def filter(fonction:A => Boolean):Stream[A]
+    def filter(fonction: A => Boolean): Stream[A]
 
-    final def union[B >: A](stream: => Stream[B]):Stream[B]= {
+    final def union[B >: A](stream: => Stream[B]): Stream[B] = {
       this match {
-        case cons:Cons[A] => Cons(cons.head, ???)
-        case EmptyStream => ???
+        case cons: Cons[A] => Cons(cons.head, cons.tail.union(stream))
+        case EmptyStream   => stream
       }
     }
 
-    def isEmpty:Boolean
+    def isEmpty: Boolean
 
     def take(n: Int): Stream[A] = {
       this match {
         case EmptyStream => EmptyStream
-        case cons: Cons[A] => {
+        case cons: Cons[A] =>
           if (n <= 0) EmptyStream
           else if (n == 1) Cons(cons.head, EmptyStream)
-          else Cons(cons.head, cons.tail.take((n - 1)))
-        }
+          else Cons(cons.head, cons.tail.take(n - 1))
       }
     }
 
-    def foreach(effetDeBord:A => Unit):Unit
+    def foreach(effetDeBord: A => Unit): Unit
   }
 
   object Stream {
     // 'A*' signifie var args de A
-    def apply[A](values:A*):Stream[A] = {
+    def apply[A](values: A*): Stream[A] = {
       if (values.isEmpty) {
         EmptyStream
       } else {
         // ':_*' permet d'étendre une Stream en var args
-        (Cons(values.head, Stream(values.tail:_*)))
+        Cons(values.head, Stream(values.tail: _*))
       }
     }
 
     def unapply[A](xs: Stream[A]): Option[(A, Stream[A])] = {
       xs match {
-        case EmptyStream => None
-        case cons:Cons[A] => Some((cons.head, cons.tail))
+        case EmptyStream   => None
+        case cons: Cons[A] => Some((cons.head, cons.tail))
       }
     }
   }
-  /**
-   * Cons veut dire Constructor, c'est lui qui permet de construire la Stream en ajoutant un élément
-   * à la queue
-   */
-  final class Cons[A](val head:A, tl: => Stream[A]) extends  Stream[A] {
 
+  /**
+    * Cons veut dire Constructor, c'est lui qui permet de construire la Stream en ajoutant un élément
+    * à la queue
+    */
+  final class Cons[A](val head: A, tl: => Stream[A]) extends Stream[A] {
 
     def isEmpty = false
 
     // ce mécanisme permet de garantir la lazyness de la queue de la stream
     // ainsi que la mémoization des valeurs accédées
-    lazy val tail:Stream[A] = tl
+    lazy val tail: Stream[A] = tl
 
-    def map[B](fonction:A => B):Stream[B] = new Cons(fonction(head), tail.map(fonction))
-
+    def map[B](fonction: A => B): Stream[B] =
+      new Cons(fonction(head), tail.map(fonction))
 
     /**
-     * l'implémentation de flatMap a besoin d'union
-     */
-    def flatMap[B](fonction:A => Stream[B]):Stream[B] = ???
+      * l'implémentation de flatMap a besoin d'union
+      */
+    def flatMap[B](fonction: A => Stream[B]): Stream[B] =
+      fonction(head).union(tail.flatMap(fonction))
 
-    override def filter(fonction:A => Boolean):Stream[A] = ???
+    override def filter(fonction: A => Boolean): Stream[A] =
+      if (fonction(head)) Cons(head, tail.filter(fonction))
+      else tl.filter(fonction)
 
-    override def equals(that:Any):Boolean = ???
+    override def equals(that: Any): Boolean = that match {
+      case Cons(h, t) => head.equals(h) && tl.equals(t)
+      case Nil        => false
+    }
 
-    override def hashCode():Int = head.hashCode()
+    override def hashCode(): Int = head.hashCode()
 
-    override def foreach(effetDeBord:A => Unit):Unit = {
+    override def foreach(effetDeBord: A => Unit): Unit = {
       effetDeBord(head)
       tail.foreach(effetDeBord)
     }
   }
 
   object Cons {
-    def apply[A](head:A, tl: => Stream[A]) = new Cons(head,tl)
+    def apply[A](head: A, tl: => Stream[A]) = new Cons(head, tl)
 
-    def unapply[A](cons:Cons[A]) = Stream.unapply(cons)
+    def unapply[A](cons: Cons[A]): Option[(A, Stream[A])] = Stream.unapply(cons)
   }
 
   /**
-   * il y a qu'un seul EmptyStream, donc cela peut être un case object
-   */
+    * il y a qu'un seul EmptyStream, donc cela peut être un case object
+    */
   case object EmptyStream extends Stream[Nothing] {
     type A = Nothing
 
-    def map[B](fonction:A => B):Stream[B]  = ???
+    def map[B](fonction: A => B): Stream[B] = this
 
-    def flatMap[B](fonction:A => Stream[B]):Stream[B] = ???
+    def flatMap[B](fonction: A => Stream[B]): Stream[B] = this
 
-    def filter(fonction:A => Boolean):Stream[A] = ???
+    def filter(fonction: A => Boolean): Stream[A] = this
 
     def isEmpty: Boolean = true
 
-    def foreach(effetDeBord:A => Unit):Unit = {}
+    def foreach(effetDeBord: A => Unit): Unit = {}
   }
 
   exercice("création") {
 
     Stream() should be(EmptyStream)
 
-    Stream(1,2,3) should be(Cons(1,Cons(2,Cons(3,EmptyStream))))
+    Stream(1, 2, 3) should be(Cons(1, Cons(2, Cons(3, EmptyStream))))
 
   }
 
   exercice("map") {
-    Stream(1,2,3).map(x => x + 1) should be(Stream(2,3,4))
+    Stream(1, 2, 3).map(x => x + 1) should be(Stream(2, 3, 4))
   }
-
 
   exercice("union") {
 
-    Stream(1,2,3).union(Stream(4,5)) should be(Stream(1,2,3,4,5))
+    Stream(1, 2, 3).union(Stream(4, 5)) should be(Stream(1, 2, 3, 4, 5))
 
-    Stream(1,2,3).union(Stream("A","B","C")) should be(Stream(1,2,3,"A","B","C"))
+    Stream(1, 2, 3).union(Stream("A", "B", "C")) should be(
+      Stream(1, 2, 3, "A", "B", "C"))
     // Cet example est complexe, le compilateur cherche un B pour l'union tel que
     // String <: B
     // A (ici Int) <: B
@@ -138,22 +143,23 @@ class e1_bonus_stream extends HandsOnSuite {
 
   exercice("flatMap") {
 
-    val combinaison = for (a <- Stream("A","B"); i <- Stream(1,2)) yield (a + i)
+    val combinaison = for (a <- Stream("A", "B"); i <- Stream(1, 2))
+      yield (a + i)
 
-    combinaison should be( Stream("A1","A2","B1","B2"))
+    combinaison should be(Stream("A1", "A2", "B1", "B2"))
 
   }
 
   exercice("filter") {
 
-    Stream(1,2,3).filter(x => x > 1) should be(Stream(2,3))
+    Stream(1, 2, 3).filter(x => x > 1) should be(Stream(2, 3))
 
-    Stream(1,2,3).filter(x => false) should be(EmptyStream)
+    Stream(1, 2, 3).filter(x => false) should be(EmptyStream)
   }
 
   exercice("lazyness") {
 
-    val s = Stream(1,2,3).map{
+    val s = Stream(1, 2, 3).map {
       case 1 => 1
       case _ => {
         throw new Exception("I should be lazy")
@@ -162,21 +168,19 @@ class e1_bonus_stream extends HandsOnSuite {
 
     assert(!s.take(1).isEmpty)
 
-
   }
 
   exercice("lazyness 2") {
-    val s:Stream[Int] = Stream(1,2,3).map{
+    val s: Stream[Int] = Stream(1, 2, 3).map {
       case 1 => 1
       case _ => {
         throw new Exception("I should be lazy")
       }
     }
 
-    val s2 = for (i <- s; j <- s) yield(i + j)
+    val s2 = for (i <- s; j <- s) yield (i + j)
 
     s2.take(1).foreach(println)
   }
-
 
 }
